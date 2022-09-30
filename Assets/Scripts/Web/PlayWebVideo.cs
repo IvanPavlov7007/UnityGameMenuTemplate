0e06938bf1b047be86832201c088ceef74745868
IvanPavlov <ivanpavlov7007@gmail.com>
2022-09-30 13:35:26 +0200

    Trying a webGL fix

---
+++ Assets/Scripts/Web/PlayWebVideo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class PlayWebVideo : MonoBehaviour
+{
+    [SerializeField]
+    string fileName;
+    
+    VideoPlayer videoPlayer;
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath,fileName);
+        videoPlayer.Play();
+    }
+}
